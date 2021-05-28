581efee5b7b1c46300d55d06efccdd36dd49e641
kss4037 <49977437+kss4037@users.noreply.github.com>
2021-05-28 18:21:22 +0900

    Merge

---
+++ src/LabelDecorate/LabelDecorator.java
@@ -0,0 +1,26 @@
+package LabelDecorate;
+
+import javax.swing.JLabel;
+
+public class LabelDecorator extends LabelComponent {
+
+
+	protected String text;
+
+
+	public LabelDecorator(JLabel label) {
+		this.label = label;
+	}
+	public LabelDecorator(String text) {
+		super(text);
+	}
+	public LabelDecorator() {}
+
+	@Override
+	public void decorate() {
+		
+	}
+	
+	
+
+}
