be0f88c9217bd214f766e3182cec63671ae69e6e
8750cut <48307855+8750cut@users.noreply.github.com>
2021-05-28 13:02:05 +0900

    Update CustomerInfo.java

--- src/ui/admin/CustomerInfo.java
+++ src/ui/admin/CustomerInfo.java
@@ -1,7 +1,7 @@
 package ui.admin;
 
-import adminFactory_sh.JButtonCreator;
-import adminFactory_sh.JLabelCreator;
+import adminFactory_jy.JButtonCreator;
+import adminFactory_jy.JLabelCreator;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
@@ -55,6 +55,8 @@ import java.awt.event.FocusEvent;
 
 public class CustomerInfo extends JFrame {
 
+	private JButtonCreator btncreator;
+	private JLabelCreator labelcreator;
    private JPanel contentPane;
    private JTextField txtSearch;
    private final ButtonGroup buttonGroup = new ButtonGroup();
@@ -80,6 +82,7 @@ public class CustomerInfo extends JFrame {
       setTitle("\uD68C\uC6D0\uC815\uBCF4 \uBC0F \uAD00\uB9AC");
       setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
       setBounds(100, 100, 999, 710);
+    
       contentPane = new JPanel();
       contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
       contentPane.setLayout(new BorderLayout(0, 0));
@@ -94,10 +97,7 @@ public class CustomerInfo extends JFrame {
       contentPane.add(panel_1, BorderLayout.CENTER);
       panel_1.setLayout(null);
       
-      JLabel lblNewLabel = new JLabel("\uD68C\uC6D0 \uB9AC\uC2A4\uD2B8");
-      lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
-      lblNewLabel.setFont(new Font("¸¼Àº °íµñ", Font.BOLD, 20));
-      lblNewLabel.setBounds(97, 16, 147, 29);
+      JLabel lblNewLabel = (JLabel) labelcreator.createWithHorizontal("\uD68C\uC6D0 \uB9AC\uC2A4\uD2B8","ë§‘ì€ ê³ ë”•", 97, 16, 147, 29, 20);
       panel_1.add(lblNewLabel);
       
       JButton btnAllMembers = new JButton("\uD68C\uC6D0 \uC804\uCCB4 \uB9AC\uC2A4\uD2B8");
@@ -111,14 +111,10 @@ public class CustomerInfo extends JFrame {
       btnAllMembers.setBounds(813, 13, 147, 23);
       panel_1.add(btnAllMembers);
       
-      JScrollPane scrollPane = new JScrollPane();
-      scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
-      scrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_ALWAYS);
-      scrollPane.setBounds(256, 52, 715, 581);
-      panel_1.add(scrollPane);
-//      String [] headings = {"°ü¸® ¹øÈ£","ÀÌ¸§","¾ÆÀÌµğ","ºñ¹Ğ¹øÈ£","ÀÌ¸ŞÀÏ","ÈŞ´ëÆù ¹øÈ£","»ı³â¿ùÀÏ","¼ºº°"};
+      JScrollPane scrollPane = setScrollPane(panel_1);
+//      String [] headings = {"ê´€ë¦¬ ë²ˆí˜¸","ì´ë¦„","ì•„ì´ë””","ë¹„ë°€ë²ˆí˜¸","ì´ë©”ì¼","íœ´ëŒ€í° ë²ˆí˜¸","ìƒë…„ì›”ì¼","ì„±ë³„"};
 //                  
-//      Object [][] rowdata = new Object[mbList.size()][headings.length]; // coustomer.getUserList();  // return Object[][size = 5]µğºñ¿¡¼­  ¿ÀºêÁ§Æ® 2Â÷ ¹è¿­·Î ¿ä¼Ò°©µé ºÒ·¯¿Í¼­ ÀúÀå
+//      Object [][] rowdata = new Object[mbList.size()][headings.length]; // coustomer.getUserList();  // return Object[][size = 5]ë””ë¹„ì—ì„œ  ì˜¤ë¸Œì íŠ¸ 2ì°¨ ë°°ì—´ë¡œ ìš”ì†Œê°‘ë“¤ ë¶ˆëŸ¬ì™€ì„œ ì €ì¥
 //      showAllMemberDB();
          
 //      table = new JTable(rowdata, headings);
@@ -140,17 +136,12 @@ public class CustomerInfo extends JFrame {
             txtPhone.setText(MemberTable.getValueAt(selRow, 5).toString());
             txtBirth.setText(MemberTable.getValueAt(MemberTable.getSelectedRow(), 6).toString());
             
-            if((MemberTable.getValueAt(MemberTable.getSelectedRow(), 7) == "³²ÀÚ")) {
+            if((MemberTable.getValueAt(MemberTable.getSelectedRow(), 7) == "ë‚¨ì")) {
                rdMale.setSelected(true);
             }else {
                rdFemale.setSelected(true);
             }
-            
-            
-         
-           
-         
-            
+
          }
       });
       scrollPane.setViewportView(MemberTable);
@@ -175,135 +166,42 @@ public class CustomerInfo extends JFrame {
       panel_1.add(panel_2);
       panel_2.setLayout(null);
       
-      JLabel lblNewLabel_1 = new JLabel("\uC774\uB984");
-      lblNewLabel_1.setFont(new Font("µ¸¿ò", Font.BOLD, 14));
-      lblNewLabel_1.setHorizontalAlignment(SwingConstants.CENTER);
-      lblNewLabel_1.setBounds(12, 69, 69, 31);
+      
+      JLabel lblNewLabel_1 = (JLabel) labelcreator.createWithHorizontal("\uC774\uB984","ë‹ì›€",12, 69, 69, 31, 14);
       panel_2.add(lblNewLabel_1);
       
       txtName = new JTextField();
-      txtName.setHorizontalAlignment(SwingConstants.CENTER);
-      txtName.setBounds(93, 69, 116, 34);
-      panel_2.add(txtName);
-      txtName.setColumns(10);
+      panel_2.add(setJTextField(txtName, 93, 69, 116, 34));
       
-      JLabel label = new JLabel("\uC544\uC774\uB514");
-      label.setFont(new Font("µ¸¿ò", Font.BOLD, 14));
-      label.setHorizontalAlignment(SwingConstants.CENTER);
-      label.setBounds(12, 125, 69, 31);
+      JLabel label = (JLabel) labelcreator.createWithHorizontal("\uC544\uC774\uB514","ë‹ì›€", 12, 125, 69, 31, 14);
       panel_2.add(label);
       
       txtLogin = new JTextField();
-      txtLogin.setHorizontalAlignment(SwingConstants.CENTER);
-      txtLogin.setColumns(10);
-      txtLogin.setBounds(93, 125, 116, 34);
-      panel_2.add(txtLogin);
+      panel_2.add(setJTextField(txtLogin, 93, 125, 116, 34));
       
-      JLabel label_1 = new JLabel("\uD328\uC2A4\uC6CC\uB4DC");
-      label_1.setFont(new Font("µ¸¿ò", Font.BOLD, 14));
-      label_1.setHorizontalAlignment(SwingConstants.CENTER);
-      label_1.setBounds(12, 191, 69, 31);
+      JLabel label_1 = (JLabel) labelcreator.createWithHorizontal("\uD328\uC2A4\uC6CC\uB4DC","ë‹ì›€", 12, 191, 69, 31, 14);
       panel_2.add(label_1);
       
       txtPw = new JTextField();
-      txtPw.setHorizontalAlignment(SwingConstants.CENTER);
-      txtPw.setColumns(10);
-      txtPw.setBounds(93, 191, 116, 34);
-      panel_2.add(txtPw);
+      panel_2.add(setJTextField(txtPw, 93, 191, 116, 34));
       
-      JLabel label_2 = new JLabel("\uC774\uBA54\uC77C");
-      label_2.setFont(new Font("µ¸¿ò", Font.BOLD, 14));
-      label_2.setHorizontalAlignment(SwingConstants.CENTER);
-      label_2.setBounds(12, 248, 69, 31);
+      JLabel label_2 = (JLabel) labelcreator.createWithHorizontal("\uC774\uBA54\uC77C","ë‹ì›€",12, 248, 69, 31, 14);
       panel_2.add(label_2);
       
       txtEmail = new JTextField();
-      txtEmail.setHorizontalAlignment(SwingConstants.CENTER);
-      txtEmail.setColumns(10);
-      txtEmail.setBounds(93, 248, 116, 34);
-      panel_2.add(txtEmail);
+      panel_2.add(setJTextField(txtEmail, 93, 248, 116, 34));
       
-      JLabel label_3 = new JLabel("\uD734\uB300\uD3F0");
-      label_3.setFont(new Font("µ¸¿ò", Font.BOLD, 14));
-      label_3.setHorizontalAlignment(SwingConstants.CENTER);
-      label_3.setBounds(12, 309, 69, 31);
+      JLabel label_3 = (JLabel) labelcreator.createWithHorizontal("\uD734\uB300\uD3F0","ë‹ì›€",12, 309, 69, 31, 14);
       panel_2.add(label_3);
+
+      panel_2.add(setPhoneJTextField("-\uB97C\uBE7C\uACE0\uC785\uB825",93, 309, 116, 34));
       
-      txtPhone = new JTextField();
-      txtPhone.setText("-\uB97C\uBE7C\uACE0\uC785\uB825");
-      txtPhone.addFocusListener(new FocusAdapter() {
-			@Override
-			public void focusGained(FocusEvent e) {
-				txtPhone.setForeground(Color.BLACK);
-				txtPhone.setBackground(Color.YELLOW);
-				String phone = txtPhone.getText();
-				
-				if( phone.equals("-¸¦»©°íÀÔ·Â") )
-					txtPhone.setText("");
-			}			
-			@Override
-			public void focusLost(FocusEvent e) {
-				txtPhone.setBackground(Color.WHITE);
-				txtPhone.setForeground(Color.BLACK);
-				String phone = txtPhone.getText();
-				if(phone.contains("-"))
-				{
-					JOptionPane.showMessageDialog(null, "-¸¦»©°íÀÔ·Â");
-					txtPhone.setText("-¸¦»©°íÀÔ·Â");
-				}
-				if( phone.isEmpty() ) {
-					txtPhone.setText("-¸¦»©°íÀÔ·Â");
-				}
-			}
-		});
-      txtPhone.setHorizontalAlignment(SwingConstants.CENTER);
-      txtPhone.setColumns(10);
-      txtPhone.setBounds(93, 309, 116, 34);
-      panel_2.add(txtPhone);
-      
-      JLabel label_4 = new JLabel("\uC0DD\uB144\uC6D4\uC77C");
-      label_4.setFont(new Font("µ¸¿ò", Font.BOLD, 14));
-      label_4.setHorizontalAlignment(SwingConstants.CENTER);
-      label_4.setBounds(12, 372, 69, 31);
+      JLabel label_4 = (JLabel) labelcreator.createWithHorizontal("\uC0DD\uB144\uC6D4\uC77C","ë‹ì›€",12, 372, 69, 31, 14);
       panel_2.add(label_4);
       
-      txtBirth = new JTextField();
-      txtBirth.setText("yyyymmdd\uD615\uC2DD");
-      txtBirth.addFocusListener(new FocusAdapter() {
-			@Override
-			public void focusGained(FocusEvent e) {
-				txtBirth.setForeground(Color.BLACK);
-				txtBirth.setBackground(Color.YELLOW);
-				String birth = txtBirth.getText();
-				if( birth.equals("yyyymmddÇü½Ä")) {
-					txtBirth.setText("");
-				} 
-					
-			}			
-			@Override
-			public void focusLost(FocusEvent e) {
-				txtBirth.setBackground(Color.WHITE);
-				txtBirth.setForeground(Color.BLACK);
-				String birth = txtBirth.getText();
-				if(birth.contains("-") || birth.toCharArray().length>=9 ||birth.toCharArray().length <=7) {
-					JOptionPane.showMessageDialog(null, "yyyymmddÇü½Ä¿¡ ¸ÂÁö ¾Ê½À´Ï´Ù");
-					txtBirth.setText("yyyymmddÇü½Ä");
-				}
-					
-				if( birth.isEmpty() ) {
-					txtBirth.setText("yyyymmddÇü½Ä");
-				}
-			}
-		});
-      txtBirth.setHorizontalAlignment(SwingConstants.CENTER);
-      txtBirth.setColumns(10);
-      txtBirth.setBounds(93, 372, 116, 34);
-      panel_2.add(txtBirth);
+      panel_2.add(setBirthJTextField("yyyymmdd\uD615\uC2DD",93, 372, 116, 34));
       
-      JLabel label_5 = new JLabel("\uC131\uBCC4");
-      label_5.setFont(new Font("µ¸¿ò", Font.BOLD, 14));
-      label_5.setHorizontalAlignment(SwingConstants.CENTER);
-      label_5.setBounds(12, 429, 69, 31);
+      JLabel label_5 = (JLabel) labelcreator.createWithHorizontal("\uC131\uBCC4","ë‹ì›€",12, 429, 69, 31, 14);
       panel_2.add(label_5);
       
        rdMale = new JRadioButton("\uB0A8\uC790");
@@ -325,7 +223,6 @@ public class CustomerInfo extends JFrame {
       btnUpdateMember.setBounds(24, 530, 92, 23);
       panel_2.add(btnUpdateMember);
       
-      
       JButton btnAddMember = new JButton("\uD68C\uC6D0\uCD94\uAC00");
       btnAddMember.setBounds(24, 497, 92, 23);
       panel_2.add(btnAddMember);
@@ -334,17 +231,9 @@ public class CustomerInfo extends JFrame {
       btnDeleteMember.setBounds(128, 530, 92, 23);
       panel_2.add(btnDeleteMember);
       
-      txtId = new JTextField();
-      txtId.setEditable(false);
-      txtId.setHorizontalAlignment(SwingConstants.CENTER);
-      txtId.setColumns(10);
-      txtId.setBounds(93, 9, 116, 34);
-      panel_2.add(txtId);
+      panel_2.add(setIdJTextField(93, 9, 116, 34));
       
-      JLabel label_6 = new JLabel("\uAD00\uB9AC\uBC88\uD638");
-      label_6.setHorizontalAlignment(SwingConstants.CENTER);
-      label_6.setFont(new Font("µ¸¿ò", Font.BOLD, 14));
-      label_6.setBounds(12, 11, 69, 31);
+      JLabel label_6 = (JLabel) labelcreator.createWithHorizontal("\uAD00\uB9AC\uBC88\uD638","ë‹ì›€",12, 11, 69, 31, 14);
       panel_2.add(label_6);
       
       JButton btnSearch = new JButton("\uAC80\uC0C9");
@@ -361,7 +250,7 @@ public class CustomerInfo extends JFrame {
 //      showSpecificMember(txtSearch.getText());
       
       JLabel lblNewLabel_2 = new JLabel("\uD68C\uC6D0 \uAC80\uC0C9");
-      lblNewLabel_2.setFont(new Font("HY³ª¹«B", Font.BOLD, 18));
+      lblNewLabel_2.setFont(new Font("HYë‚˜ë¬´B", Font.BOLD, 18));
       lblNewLabel_2.setBackground(Color.BLACK);
       lblNewLabel_2.setHorizontalAlignment(SwingConstants.CENTER);
       lblNewLabel_2.setBounds(260, 13, 114, 23);
@@ -384,11 +273,11 @@ public class CustomerInfo extends JFrame {
             System.out.println(login);
             boolean b =mgr.deleteOneMember(id, login);
             if (b) {
-               JOptionPane.showMessageDialog(null, login+"È¸¿ø »èÁ¦ ¼º°ø");
+               JOptionPane.showMessageDialog(null, login+"íšŒì› ì‚­ì œ ì„±ê³µ");
             }else {
-//               System.out.println("È¸¿ø »èÁ¦ ½ÇÆĞ ");
+//               System.out.println("íšŒì› ì‚­ì œ ì‹¤íŒ¨ ");
                
-               JOptionPane.showMessageDialog(null, login+"È¸¿ø »èÁ¦ ½ÇÆĞ");
+               JOptionPane.showMessageDialog(null, login+"íšŒì› ì‚­ì œ ì‹¤íŒ¨");
             }
          }
       });
@@ -409,17 +298,14 @@ public class CustomerInfo extends JFrame {
 //             MemberDBMgr mgr = new MemberDBMgr();
             
              if(mb != null) {
-            	 JOptionPane.showMessageDialog(null, login+"È¸¿ø°¡ÀÔ Ãß°¡¼º°ø");
+            	 JOptionPane.showMessageDialog(null, login+"íšŒì›ê°€ì… ì¶”ê°€ì„±ê³µ");
             	 mgr.AddOneMember(mb);
                 
                   
-             }else {
-            	 JOptionPane.showMessageDialog(null, login+"È¸¿ø°¡ÀÔ Ãß°¡½ÇÆĞ");
-                System.out.println("¿Ã¹Ù¸¥ Á¤º¸¸¦ ±âÀÔÇØ ÁÖ¼¼¿ä!");
+             }else{
+            	 JOptionPane.showMessageDialog(null, login+"íšŒì›ê°€ì… ì¶”ê°€ì‹¤íŒ¨");
+            	 System.out.println("ì˜¬ë°”ë¥¸ ì •ë³´ë¥¼ ê¸°ì…í•´ ì£¼ì„¸ìš”!");
              }
-            
-             
-            
          }
       });
       btnUpdateMember.addActionListener(new ActionListener() {
@@ -436,11 +322,11 @@ public class CustomerInfo extends JFrame {
              Member mb = new Member(id,name, login, pw, gender, birth, email, phone);
              boolean b = mgr.editOneMember(mb);
              if(b) { 
-                System.out.println(name +" ´ÔÀÇ Á¤º¸¸¦ ¼öÁ¤Çß½À´Ï´Ù");
-                JOptionPane.showMessageDialog(null, name +" ´ÔÀÇ Á¤º¸¸¦ ¼öÁ¤Çß½À´Ï´Ù");
+                System.out.println(name +" ë‹˜ì˜ ì •ë³´ë¥¼ ìˆ˜ì •í–ˆìŠµë‹ˆë‹¤");
+                JOptionPane.showMessageDialog(null, name +" ë‹˜ì˜ ì •ë³´ë¥¼ ìˆ˜ì •í–ˆìŠµë‹ˆë‹¤");
              } else {
-                System.out.println(name +" ´ÔÀÇ Á¤º¸¸¦ ¼öÁ¤ ½ÇÆĞ ÇÏ¿´½À´Ï´Ù");
-                JOptionPane.showMessageDialog(null, name +" ´ÔÀÇ Á¤º¸¸¦ ¼öÁ¤ ½ÇÆĞ ÇÏ¿´½À´Ï´Ù");
+                System.out.println(name +" ë‹˜ì˜ ì •ë³´ë¥¼ ìˆ˜ì • ì‹¤íŒ¨ í•˜ì˜€ìŠµë‹ˆë‹¤");
+                JOptionPane.showMessageDialog(null, name +" ë‹˜ì˜ ì •ë³´ë¥¼ ìˆ˜ì • ì‹¤íŒ¨ í•˜ì˜€ìŠµë‹ˆë‹¤");
              }
              
             
@@ -454,8 +340,8 @@ public class CustomerInfo extends JFrame {
             txtLogin.setText("");
              txtPw.setText("");
              txtEmail.setText("");
-             txtPhone.setText("-¸¦»©°íÀÔ·Â");
-             txtBirth.setText("yyyymmddÇü½Ä");
+             txtPhone.setText("-ë¥¼ë¹¼ê³ ì…ë ¥");
+             txtBirth.setText("yyyymmddí˜•ì‹");
              btnGroup.clearSelection();
 //             rdFemale.setSelected(false);
 //             rdMale.setSelected(false);
@@ -465,8 +351,103 @@ public class CustomerInfo extends JFrame {
       });
       
    }
+
+public JTextField setIdJTextField( int x, int y, int z, int w) {
+	txtId = new JTextField();
+      txtId.setEditable(false);
+      txtId.setHorizontalAlignment(SwingConstants.CENTER);
+      txtId.setColumns(10);
+      txtId.setBounds(93, 9, 116, 34);
+      return txtId;
+}
+
+public JTextField setPhoneJTextField(String text, int x, int y, int z, int w) {
+	txtPhone = new JTextField();
+      txtPhone.setText("-\uB97C\uBE7C\uACE0\uC785\uB825");
+      txtPhone.addFocusListener(new FocusAdapter() {
+			@Override
+			public void focusGained(FocusEvent e) {
+				txtPhone.setForeground(Color.BLACK);
+				txtPhone.setBackground(Color.YELLOW);
+				String phone = txtPhone.getText();
+				
+				if( phone.equals("-ë¥¼ë¹¼ê³ ì…ë ¥") )
+					txtPhone.setText("");
+			}			
+			@Override
+			public void focusLost(FocusEvent e) {
+				txtPhone.setBackground(Color.WHITE);
+				txtPhone.setForeground(Color.BLACK);
+				String phone = txtPhone.getText();
+				if(phone.contains("-"))
+				{
+					JOptionPane.showMessageDialog(null, "-ë¥¼ë¹¼ê³ ì…ë ¥");
+					txtPhone.setText("-ë¥¼ë¹¼ê³ ì…ë ¥");
+				}
+				if( phone.isEmpty() ) {
+					txtPhone.setText("-ë¥¼ë¹¼ê³ ì…ë ¥");
+				}
+			}
+		});
+      txtPhone.setHorizontalAlignment(SwingConstants.CENTER);
+      txtPhone.setColumns(10);
+      txtPhone.setBounds(93, 309, 116, 34);
+      return txtPhone;
+}
+
+public JTextField setBirthJTextField(String text,int x, int y, int z, int w) {
+	txtBirth = new JTextField();
+      txtBirth.setText("yyyymmdd\uD615\uC2DD");
+      txtBirth.addFocusListener(new FocusAdapter() {
+			@Override
+			public void focusGained(FocusEvent e) {
+				txtBirth.setForeground(Color.BLACK);
+				txtBirth.setBackground(Color.YELLOW);
+				String birth = txtBirth.getText();
+				if( birth.equals("yyyymmddí˜•ì‹")) {
+					txtBirth.setText("");
+				} 
+					
+			}			
+			@Override
+			public void focusLost(FocusEvent e) {
+				txtBirth.setBackground(Color.WHITE);
+				txtBirth.setForeground(Color.BLACK);
+				String birth = txtBirth.getText();
+				if(birth.contains("-") || birth.toCharArray().length>=9 ||birth.toCharArray().length <=7) {
+					JOptionPane.showMessageDialog(null, "yyyymmddí˜•ì‹ì— ë§ì§€ ì•ŠìŠµë‹ˆë‹¤");
+					txtBirth.setText("yyyymmddí˜•ì‹");
+				}
+					
+				if( birth.isEmpty() ) {
+					txtBirth.setText("yyyymmddí˜•ì‹");
+				}
+			}
+		});
+      txtBirth.setHorizontalAlignment(SwingConstants.CENTER);
+      txtBirth.setColumns(10);
+      txtBirth.setBounds(93, 372, 116, 34);
+      return txtBirth;
+}
+   
+public JTextField setJTextField(JTextField txt, int x, int y, int z, int w) {
+      txt.setHorizontalAlignment(SwingConstants.CENTER);
+      txt.setBounds(x, y, z, w);
+      txt.setColumns(10);
+      return txt;
+}
+
+private JScrollPane setScrollPane(JPanel panel_1) {
+	JScrollPane scrollPane = new JScrollPane();
+      scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+      scrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_ALWAYS);
+      scrollPane.setBounds(256, 52, 715, 581);
+      panel_1.add(scrollPane);
+	return scrollPane;
+}
+
    public void showSpecificMember(String target) {
-      final String [] columnNames = {"°ü¸® ¹øÈ£","ÀÌ¸§","¾ÆÀÌµğ","ºñ¹Ğ¹øÈ£","ÀÌ¸ŞÀÏ","ÈŞ´ëÆù ¹øÈ£","»ı³â¿ùÀÏ","¼ºº°"};
+      final String [] columnNames = {"ê´€ë¦¬ ë²ˆí˜¸","ì´ë¦„","ì•„ì´ë””","ë¹„ë°€ë²ˆí˜¸","ì´ë©”ì¼","íœ´ëŒ€í° ë²ˆí˜¸","ìƒë…„ì›”ì¼","ì„±ë³„"};
       
       //DB 
 
@@ -482,7 +463,7 @@ public class CustomerInfo extends JFrame {
          data[i][4] = mb.getEmail();
          data[i][5] = mb.getPhone();
          data[i][6] = mb.getBirth();
-         data[i][7] = mb.getGender() == 1 ? "³²ÀÚ":"¿©ÀÚ";
+         data[i][7] = mb.getGender() == 1 ? "ë‚¨ì":"ì—¬ì";
          }
       
       
@@ -494,10 +475,7 @@ public class CustomerInfo extends JFrame {
       MemberTable.setRowSorter(trs);
 
       trs.setRowFilter(RowFilter.regexFilter(target));
-      
-
-      
-      
+ 
    }
    public void searchMember(String target) {
       TableRowSorter<TableModel> trs=new TableRowSorter<TableModel>( MemberTable.getModel());
@@ -508,7 +486,7 @@ public class CustomerInfo extends JFrame {
    }
    
    public void showAllMemberDB() {
-      final String [] columnNames = {"°ü¸® ¹øÈ£","ÀÌ¸§","¾ÆÀÌµğ","ºñ¹Ğ¹øÈ£","ÀÌ¸ŞÀÏ","ÈŞ´ëÆù ¹øÈ£","»ı³â¿ùÀÏ","¼ºº°"};
+      final String [] columnNames = {"ê´€ë¦¬ ë²ˆí˜¸","ì´ë¦„","ì•„ì´ë””","ë¹„ë°€ë²ˆí˜¸","ì´ë©”ì¼","íœ´ëŒ€í° ë²ˆí˜¸","ìƒë…„ì›”ì¼","ì„±ë³„"};
       
       //DB 
 
@@ -524,7 +502,7 @@ public class CustomerInfo extends JFrame {
          data[i][4] = mb.getEmail();
          data[i][5] = mb.getPhone();
          data[i][6] = mb.getBirth();
-         data[i][7] = mb.getGender() == 1 ? "³²ÀÚ":"¿©ÀÚ";
+         data[i][7] = mb.getGender() == 1 ? "ë‚¨ì":"ì—¬ì";
          }
       
       
